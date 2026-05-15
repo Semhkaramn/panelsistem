de6bbce4da6acf9e3747fb83813fdb3173a72d42
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-05-16 01:27:39 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,15 +3,9 @@
 """
 
 from handlers.callbacks import handle_callback
-from handlers.commands import (
-    start_command, randy_command, ben_command,
-    bitir_command, number_command
-)
 from handlers.messages import handle_message
 
 __all__ = [
     'handle_callback',
-    'start_command', 'randy_command', 'ben_command',
-    'bitir_command', 'number_command',
     'handle_message'
 ]
